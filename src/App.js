a9c55c149b8593a881cbc795aef4dd76f93f8504
Kaushik Deo <kaushikmdeo@gmail.com>
2023-12-11 03:20:57 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import { ApolloProvider, useQuery} from '@apollo/client';
+import {apolloClient} from './graphql/client'
+import { GET_BOOKS } from './graphql/queries/booksQueries';
 
 function App() {
+  const { loading, error, data } = useQuery(GET_BOOKS, { errorPolicy: "all" });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={apolloClient}>
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Hello World
+          </p>
+        </header>
+      </div>
+    </ApolloProvider>
   );
 }
 
